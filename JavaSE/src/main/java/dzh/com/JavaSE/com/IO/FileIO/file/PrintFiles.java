e5bdcbe7e5a3fdd057bf289b3ab9b4cc76f95908
邓志鸿 <1353976012@qq.com>
2024-09-17 20:38:37 +0800

    17

--- JavaSE/src/main/java/dzh/com/JavaSE/com/IO/FileIO/file/PrintFiles.java
+++ JavaSE/src/main/java/dzh/com/JavaSE/com/IO/FileIO/file/PrintFiles.java
@@ -3,25 +3,27 @@ package dzh.com.JavaSE.com.IO.FileIO.file;
 import java.io.File;
 import java.util.Arrays;
 
-public class filter {
+public class PrintFiles {
 
     public static void main(String[] args) {
-        String path="D:/idea/bjsxt/src/com/io/file";
-        File file=new File(path);
+        String path = "D:\\temp";
+        File file = new File(path);
         printName(file);
         //根路径
-        File[] roots=File.listRoots();
+        File[] roots = File.listRoots();
         System.out.println(Arrays.toString(roots));
     }
-    //打印每个文件夹下的每个文件
-    public static void printName(File file){
 
-        if (null==file||!file.exists()){
+    //打印每个文件夹下的每个文件
+    public static void printName(File file) {
+
+        if (null == file || !file.exists()) {
             return;
         }
         System.out.println(file.getAbsolutePath());
-        if (file.isDirectory()){
-            for ( File f:file.listFiles()) {
+        if (file.isDirectory()) {
+            // 不读取隐藏文件
+            for (File f : file.listFiles(o -> !o.isHidden())) {
                 printName(f);
             }
         }
